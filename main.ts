db9ab0ef4792a9e24392016c60cddfbfd417b8c0
elgatodelovecraft <112429511+elgatodelovecraft@users.noreply.github.com>
2022-09-01 11:48:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let A = 0
+let Texto = ""
+input.onButtonPressed(Button.A, function () {
+    A = randint(0, 10)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Texto = "Gato"
+})
